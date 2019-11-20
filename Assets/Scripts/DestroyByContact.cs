c46a2c8e9b2e148370042c46fd98380523d6dc82
Chadwick Lapis <c_lapis4@hotmail.com>
2019-11-20 02:47:15 -0500

    first prototype of platformer game

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject death;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Instantiate(death, other.gameObject.GetComponent<Rigidbody2D>().position, this.transform.rotation);
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player_move.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+    public float movex, speed = 0.5f, jumpspeed = 1;
+    public bool faceright = true;
+    private bool allowmove = true;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (allowmove == true)
+        {
+            movex = Input.GetAxis("Horizontal");
+
+            animator.SetFloat("speed", Mathf.Abs(movex));
+
+            if (movex > 0 && faceright == false)
+            {
+                flip();
+            }
+            else if (movex < 0 && faceright == true)
+            {
+                flip();
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump();
+            }
+            rBody.velocity = new Vector2(movex * speed, rBody.velocity.y);
+        }
+
+        if (rBody.velocity.y > 0)
+        {
+            animator.SetBool("joping", true);
+        }
+        else
+        {
+            animator.SetBool("joping", false);
+        }
+
+        if (rBody.velocity.y < 0)
+        {
+            animator.SetBool("fall", true);
+        }
+        else
+        {
+            animator.SetBool("fall", false);
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        allowmove = false;
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        allowmove = true;
+    }
+    void jump()
+    {
+        rBody.velocity = new Vector2(rBody.velocity.x, jumpspeed);
+    }
+    void flip()
+    {
+        faceright = !faceright;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
